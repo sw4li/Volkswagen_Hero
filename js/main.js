ef3568a0cb78ac1502fdb87be2c330081570c739
muhammed swalih <swalih22muhammed@gmail.com>
2023-05-27 19:56:10 +0530

    only index file

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 0,
     loop:true,
     speed:2000,
-    // autoplay: {
-    //     delay: 4000,
-    //   },
+    autoplay: {
+        delay: 4000,
+      },
    
     pagination: {
       el: ".swiper-pagination",
